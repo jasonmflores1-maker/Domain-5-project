ca9c8b58ffb6fc4b8515be27a071ae8d40b4c2b3
jasonmflores1-maker <jasonmflores1@gmail.com>
2026-02-05 09:45:57 -0500

    updated 2/5

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
-console.log("js console");
+console.log("js started");
 
 var data;
 var grid = document.querySelector(".grid-container");
 
-var xhttp = new XMLHttpRequest();
+// LOAD DATA (localStorage first, otherwise XHR)
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  if (grid) {
+    makeCards();
+  }
+} else {
+  var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-if (xhttp.readyState === 4 && xhttp.status === 200) {
-   data = JSON.parse(xhttp.responseText);
-   console.log(data);
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from gameData.json");
 
-   localStorage.setItem("data-list",JSON.stringify(data));
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter data to localStorage");
 
-   data.forEach(function(movie) {
+      if (grid) {
+        makeCards();
+      }
+    }
+  };
+
+  xhttp.open("GET", "list.JSON", true);
+  xhttp.send();
+}
+
+// RENDER CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (game) {
     let card = document.createElement("div");
     card.classList.add("card");
 
     let textData =
-
-    "<div class='movie-title'>" + movie.title + "</div>" + 
-    "<span>" +
-    "Director:" + movie.director + "<br>" +
-    " Release Date:" + movie.releaseDate + "<br>";
-    
+      "<div> Movie: " + game.title + "</div>" +
+      "<div>Director: " + game.director + "</div>" +
+      "<div>Release Date: " + game.releaseDate + "</div>";
 
     card.innerHTML = textData;
-
-    if (movie.imgSrc) {
-        card.style.backgroundImage = "url(" + movie.imgSrc + ")";
-    }
-      
     grid.appendChild(card);
-  
-    });
+  });
 
+  console.log("cards refreshed");
+}
+
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title-input");
+var directorInput = document.querySelector("#director-input");
+var releasedateInput = document.querySelector("#release-date-input");
+
+ if(form) {
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var newObj = {
+    title: titleInput.value,
+    director: directorInput.value,
+    releaseDate: releasedateInput.value
+  };
+
+  data.push(newObj);
+  localStorage.setItem("datalist", JSON.stringify(data));
+  console.log("Saved new item to localStorage");
+
+  // Only render if grid exists on this page
+  if (document.querySelector(".grid-container")) {
+    makeCards();
   }
-};
 
-xhttp.open("GET", "list.JSON", true);
-xhttp.send();
-
-form.addEventListener("submit",function(e){
-let title = titleInput.value;
-let director = directorInput.value;
-let releaseDate = releaseDateInput.value;
-let imgSrc = imgInput.value;
-let newObj = {
-  "title": title,
-  "director": director,
-  "releaseDate": releaseDate,
-  "imgSrc": imgSrc, };
-  submitData(newObj);
   form.reset();
 });
+ }
 
- 
